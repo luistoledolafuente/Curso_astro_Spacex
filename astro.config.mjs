50a0af8ab6435ab13f238c5564456df3a19088d6
luistoledolafuente <luismiguel.tlf@gmail.com>
2025-08-03 16:17:01 -0500

    mejorando para el despligue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+  adapter: vercel({}),
+  integrations: [tailwind()],
   output: 'server'
 });
